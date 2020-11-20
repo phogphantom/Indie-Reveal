ad7c0e4071a74d3334571537836e0110836595e3
phogphantom <tomstandley23@gmail.com>
2020-11-20 13:05:48 -0600

    Added in handlebars, trying to figure out how to link from one page to another with correct game

--- js/main.indie-reveal.js
+++ js/main.indie-reveal.js
@@ -1,3 +1,84 @@
+var request1 = new XMLHttpRequest();
+var request2 = new XMLHttpRequest();
+var gameTemplateData=null;
+//first request
+request1.open('GET', 'https://api.jsonbin.io/b/5f9c77559291173cbca6065d/3');
+request1.setRequestHeader("Content-Type", "application/json");
+request1.setRequestHeader("secret-key", '$2b$10$aW/Se.zAPErVkDeOY1RXBuvJpK/vqamzcPFrryVvhyT6CzGGnpMk2');
+request1.onload = function () {
+    if (request1.status >= 200 && request1.status < 400) {
+        gameTemplateData = JSON.parse(request1.responseText);
+        createHTMLGameTemplate(gameTemplateData,i)
+    } else {
+        console.log("The server returned an error.");
+    }
+};
+
+request1.onerror = function () {
+    console.log("connection error");
+};
+request1.send();
+
+//second request
+request2.open('GET','https://api.jsonbin.io/b/5f9f37c6ce4aa228955443ee/1');
+request2.setRequestHeader("Content-Type","application/json");
+request2.setRequestHeader("secret-key",'$2b$10$aW/Se.zAPErVkDeOY1RXBuvJpK/vqamzcPFrryVvhyT6CzGGnpMk2');
+request2.onload = function () {
+    if (request2.status >= 200 && request2.status < 400) {
+        var recommendData = JSON.parse(request2.responseText);
+        createHTMLRecommend(recommendData)
+    } else {
+        console.log("The server returned an error.");
+    }
+};
+request2.onerror = function () {
+    console.log("connection error");
+};
+request2.send();
+
+function createHTMLGameTemplate(gameData,i) {
+    var rawTemplate = document.getElementById("gameTemplate").innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+
+
+    var generatedHTML = compiledTemplate(gameData.games[i]);
+
+    var gameContainer = document.getElementById("game-container");
+    gameContainer.innerHTML = generatedHTML;
+
+}
+
+function createHTMLRecommend(recommendImgData) {
+    var rawRecommendTemplate = document.getElementById("recommendTemplate").innerHTML;
+    var compiledRecommendTemplate = Handlebars.compile(rawRecommendTemplate);
+    var recommendGenteratedHTML = compiledRecommendTemplate(recommendImgData);
+    var recommendContainer = document.getElementById("recommend-container");
+    recommendContainer.innerHTML = recommendGenteratedHTML;
+}
+
+
+function loadGamePage() {
+    window.location.href = "game-page-template.html";
+
+}
+/*
+document.getElementsByClassName("carousel-item").addEventListener("click",j=>{
+    if(j.target instanceof "h1"){
+        window.location.href = "game-page-template.html";
+        const i=j.target.parentElement.getAttribute("href").substring(1);
+        createHTMLGameTemplate(gameTemplateData,i);
+    }
+})
+*/
+
+
+window.addEventListener("click",e=>{
+    if(e.target instanceof Image){
+        const i=e.target.parentElement.getAttribute("href").substring(1);
+        createHTMLGameTemplate(gameTemplateData,i);
+    }
+})
+
 
 //this is the carousel control
 $('.carousel').carousel( {
