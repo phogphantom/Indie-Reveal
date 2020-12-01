4956ee54c109b05c8e0359be3fe5711f1920bd51
phogphantom <tomstandley23@gmail.com>
2020-11-30 20:57:17 -0600

    Implemented the navigation and local storage, made everything mobile friendly, making the documentation, ready to be turned in

--- js/main.indie-reveal.js
+++ js/main.indie-reveal.js
@@ -1,15 +1,19 @@
-function loadGamePage() {
+
+
+function loadGamePage(e) {
+    localStorage["game-name"]=e.target.innerText;
     window.location.href = "game-page-template.html";
 
 }
 
+/*
 window.addEventListener("click",e=>{
     if(e.target instanceof Image){
         const i=e.target.parentElement.getAttribute("href").substring(1);
         createHTMLGameTemplate(gameTemplateData,i);
     }
 })
-
+*/
 
 //this is the carousel control
 $('.carousel').carousel( {
@@ -17,56 +21,44 @@ $('.carousel').carousel( {
     pause: "hover"
 });
 
-//this is to stop video in modal and reload url on opening
-$(document).ready(function(){
-    /* Get iframe src attribute value i.e. YouTube video url
-    and store it in a variable */
-    var urlTrailerModal = $("#templateTrailer").attr('src');
-
-    /* Assign empty url value to the iframe src attribute when
-    modal hide, which stop the video playing */
-    $("#trailerModal").on('hide.bs.modal', function(){
-        $("#templateTrailer").attr('src', '');
-    });
-
-    /* Assign the initially stored url back to the iframe src
-    attribute when modal is displayed again */
-    $("#trailerModal").on('show.bs.modal', function(){
-        $("#templateTrailer").attr('src', urlTrailerModal);
-    });
-});
-
-
 //for the find a game search box
 $(document).ready(function () {
     $("#searchInput").on("keyup", function (){
-       var value = $(this).val().toLowerCase();
-       $("#listTable tr").filter(function (){
-          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-       });
+        var value = $(this).val().toLowerCase();
+        $("#listTable tr").filter(function (){
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
     });
 });
 
 
 //lightbox
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
+$(document).ready(function () {
+    const lightbox = document.createElement('div');
+    const images = document.querySelectorAll('.screen-shots img');
+    lightbox.id = 'lightbox';
+    document.body.appendChild(lightbox);
 
-const images = document.querySelectorAll('.screen-shots img');
-images.forEach(image => {
-    image.addEventListener('click', e =>{
-        lightbox.classList.add('active')
-        const img = document.createElement('img')
-        img.src = image.src
-        while (lightbox.firstChild){
-            lightbox.removeChild(lightbox.firstChild)
-        }
-        lightbox.appendChild(img)
+
+    images.forEach(image => {
+        image.addEventListener('click', e => {
+            lightbox.classList.add('active')
+            const img = document.createElement('img')
+            img.src = image.src
+            while (lightbox.firstChild) {
+                lightbox.removeChild(lightbox.firstChild)
+            }
+            lightbox.appendChild(img)
+        })
     })
-})
 
-lightbox.addEventListener('click', e =>{
-    if (e.target !== e.currentTarget) return
-    lightbox.classList.remove('active')
-})
+    lightbox.addEventListener('click', e => {
+        if (e.target !== e.currentTarget) return
+        lightbox.classList.remove('active')
+    });
+});
+
+$('#mobileHamburger').on('click',function (){
+    $('#mobile-links').toggle();
+});
+
--- js/template.indie-reveal.js
+++ js/template.indie-reveal.js
@@ -4,13 +4,27 @@ var request2 = new XMLHttpRequest();
 var gameTemplateData=null;
 */
 //first request
-request1.open('GET', 'https://api.jsonbin.io/b/5f9c77559291173cbca6065d/3');
+request1.open('GET', 'https://api.jsonbin.io/b/5f9c77559291173cbca6065d/9');
 request1.setRequestHeader("Content-Type", "application/json");
 request1.setRequestHeader("secret-key", '$2b$10$aW/Se.zAPErVkDeOY1RXBuvJpK/vqamzcPFrryVvhyT6CzGGnpMk2');
 request1.onload = function () {
     if (request1.status >= 200 && request1.status < 400) {
         gameTemplateData = JSON.parse(request1.responseText);
-        createHTMLGameTemplate(gameTemplateData,0);
+        let index=0;
+        for(let i=0;i<gameTemplateData.games.length;i++){
+            const gameName=gameTemplateData.games[i].name;
+            if(localStorage["game-name"]===gameName){
+                console.log("Match Found");
+                index=i;
+                break;
+            }
+        }
+        createHTMLGameTemplate(gameTemplateData,index);
+        $(document).ready(function () {
+           lightboxFunction();
+           videoTrailerReset();
+        });
+
 
     } else {
         console.log("The server returned an error.");
@@ -23,14 +37,23 @@ request1.onerror = function () {
 request1.send();
 
 //second request
-request2.open('GET','https://api.jsonbin.io/b/5f9f37c6ce4aa228955443ee/1');
+request2.open('GET','https://api.jsonbin.io/b/5f9f37c6ce4aa228955443ee/3');
 request2.setRequestHeader("Content-Type","application/json");
 request2.setRequestHeader("secret-key",'$2b$10$aW/Se.zAPErVkDeOY1RXBuvJpK/vqamzcPFrryVvhyT6CzGGnpMk2');
 request2.onload = function () {
     if (request2.status >= 200 && request2.status < 400) {
         var recommendData = JSON.parse(request2.responseText);
         createHTMLRecommend(recommendData);
-
+        $('.carousel').carousel( {
+            interval: 4000,
+            pause: "hover"
+        });
+        /*
+        $('.randomGames').each(function (){
+            var random = Math.floor(Math.random() * $('.recFigure',this).length);
+            $('.recFigure',this).hide().eq(random).show();
+        })
+        */
 
     } else {
         console.log("The server returned an error.");
@@ -60,3 +83,42 @@ function createHTMLRecommend(recommendImgData) {
     var recommendContainer = document.getElementById("recommend-container");
     recommendContainer.innerHTML = recommendGenteratedHTML;
 }
+
+
+
+//function for lightbox
+//this called right after the createHTML function
+function lightboxFunction() {
+    const lightbox = document.createElement('div');
+    const images = document.querySelectorAll('.game-temp-screen-shots img');
+    lightbox.id = 'lightbox';
+    document.body.appendChild(lightbox);
+    images.forEach(image => {
+        image.addEventListener('click', e => {
+            lightbox.classList.add('active')
+            const img = document.createElement('img')
+            img.src = image.src
+            while (lightbox.firstChild) {
+                lightbox.removeChild(lightbox.firstChild)
+            }
+            lightbox.appendChild(img)
+        })
+    })
+    lightbox.addEventListener('click', e => {
+        if (e.target !== e.currentTarget) return
+        lightbox.classList.remove('active')
+    });
+}
+
+
+//this is to stop video in modal and reload url on opening
+function videoTrailerReset() {
+    var urlTrailerModal = $("#templateTrailer").attr('src');//gets the iframe src
+    $("#trailerModal").on('hide.bs.modal', function () {
+        $("#templateTrailer").attr('src', '');//on modal hide it clears the src
+    });
+    $("#trailerModal").on('show.bs.modal', function () {
+        $("#templateTrailer").attr('src', urlTrailerModal);//puts in the src on show
+    });
+}
+
